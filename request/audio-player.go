9a732cc6f506193ba4219ac635373c2bdf62ff47
Scott St Germain <scottjustin5000@gmail.com>
2018-02-02 19:12:00 -0800

    adding audio and playback requests

---
+++ request/audio-player.go
@@ -0,0 +1,3 @@
+/***
+ Note: The session is included for all standard requests, but it is not included for AudioPlayer, VideoApp, or PlaybackController requests.
+***/
---
+++ request/session-ended-request.go
@@ -0,0 +1,18 @@
+package request
+
+type SessionEndedRequest struct {
+    Version  string `json:"version"`
+    Session Session `json:"session"`
+    Context Context `json:context`
+    Request struct {
+        Timestamp time.Time `json:"timestamp, string"`
+        ReqType string `json:"type"`
+        RequestId string `json:"requestId"`
+        Reason string `json:"reason"`
+        Locale string `json:"string"`
+        Error struct {
+          Type string `json:"type"`
+          Message string `json:"message"`
+        } `json:"Request"`
+    } `json:request`
+}
